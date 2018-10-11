d1f7b5abe7208a68d4c7a443ed190db4918fc590
Brandon Colchin <brandoncolchin@gmail.com>
2018-10-11 11:06:04 -0700

    trying to configure SASS & Grunt

---
+++ web/themes/custom/brandontheme/gruntfile.js
@@ -0,0 +1,15 @@
+module.exports = function(grunt){
+
+  // Configure main project settings
+  grunt.initConfig{{
+
+    // Basic settings and info about our plugins
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Name of plugin ( name of plugni without the "grunt-contrib-")
+    sass: {
+      
+    }
+
+  }};
+};
